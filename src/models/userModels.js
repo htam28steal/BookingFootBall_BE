846c9014c83151ae7ec9d05b83b22ccb34b07dd1
Tam <thanhtam0933019816@gmail.com>
2025-08-19 11:10:05 +0700

    wrong syntax

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -4,16 +4,16 @@ const UserModel = {
     getUserByEmail: async (email) => {
         try {
             const pool = await poolPromise;
-            // const result = await pool.request()
+            const result = await pool.request()
                 .input('email', sql.VarChar, email)
-                // .query('SELECT * FROM dbo.users WHERE email = @email');
+                .query('SELECT * FROM dbo.users WHERE email = @email');
 
             return result.recordset[0];
         } catch (err) {
             console.error('Error in getUserByEmail:', err);
             throw err;
         }
-    }
+    
 };
 
 module.exports = UserModel;
