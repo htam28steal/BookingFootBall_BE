5dbd82089f91be049ff56a440d23855914ed063d
Tam <thanhtam0933019816@gmail.com>
2025-08-19 10:43:08 +0700

    test CI

---
+++ src/data/mockdb.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcryptjs');
+
+const users = [
+    {
+        id: 1,
+        email: 'thanhtam123',
+        password: '123',
+    },
+    {
+        id: 2,
+        email: 'thuytrang23',
+        password: '123'
+    }
+];
+module.exports = users;
